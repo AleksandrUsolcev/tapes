1a63dd317e0a106b0dce7617290e06925f181848
Александр Усольцев <usolev@yandex.ru>
2022-08-23 18:59:20 +0300

    refactor entries views to CBV

--- tapes/entries/forms.py
+++ tapes/entries/forms.py
@@ -18,31 +18,15 @@ class EntryForm(forms.ModelForm):
     tape = forms.ModelChoiceField(label='', queryset=Tape.objects.none())
 
     def __init__(self, *args, **kwargs):
-        user_id = kwargs.pop('user_id', None)
+        user = kwargs.pop('user', None)
         super(EntryForm, self).__init__(*args, **kwargs)
-        self.fields['tape'].queryset = Tape.objects.filter(author=user_id)
+        self.fields['tape'].queryset = Tape.objects.filter(author=user)
 
 
-class TapeAddForm(forms.ModelForm):
+class TapeForm(forms.ModelForm):
     class Meta:
         model = Tape
         fields = ('title', 'slug', 'description', 'background')
-        widgets = {
-            'background': ClientsideCroppingWidget(
-                width=1000,
-                height=300,
-                preview_width=1000,
-                preview_height=300,
-            )
-        }
-
-
-class TapeEditForm(forms.ModelForm):
-    delete_tape = forms.BooleanField(label='Удалить ленту', required=False)
-
-    class Meta:
-        model = Tape
-        fields = ('description', 'background', 'slug', 'title')
         widgets = {
             'background': ClientsideCroppingWidget(
                 width=1000,
